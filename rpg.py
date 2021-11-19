07cd42d070398156108fd74c4e4ca80a71f2dc32
CheesiestMaster <74749672+CheesiestMaster@users.noreply.github.com>
2021-11-19 13:45:08 -0500

    Upload current code and Template story

---
+++ rpg.py
@@ -0,0 +1,44 @@
+import conf
+e=-1
+while True:
+    n=conf.story+"/Events/"+str(e)
+    f=open(n)
+    l=""
+    while l!='##### Begin Story #####\n':
+        l=f.readline()
+    s=""
+    while l!='##### End Story #####\n':
+        l=f.readline()
+        if l!='##### End Story #####\n':
+            i=l.lstrip("'")
+            i=i.rstrip("n'")
+            i=i.rstrip("//")
+            s=s+i
+    print(s)
+    u=input()
+    while l!='##### Begin Args #####\n':
+        l=f.readline()
+    while l!='##### End Args #####':
+        l=f.readline()
+        if l!='##### End Args #####':
+            print(l[1])
+            i=0
+            a=0
+            c=""
+            n=""
+            for i in range(len(l)):
+                if l[i]=="[":
+                    a=1
+                elif l[i]==",":
+                    a=2
+                    i+=1
+                elif l[i]=="]":
+                    a=3
+                elif a==1:
+                    c=c+l[i]
+                elif a==2:
+                    n=n+l[i]
+            if u==c:
+                e=n.lstrip()
+                
+    break
